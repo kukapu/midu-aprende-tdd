6a46f8090336ce13908499335f21f515a7301be3
Albert Alvarez Estelles <114157589+kukapu@users.noreply.github.com>
2023-06-15 12:00:21 +0200

    feat: TDD Calc en React con react-testing-library

---
+++ src/Calculator.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { evaluate } from "mathjs";
+
+export const numbers = [0,1,2,3,4,5,6,7,8,9];
+
+const rows = [
+  [7, 8, 9],
+  [4, 5, 6],
+  [1, 2, 3],
+  [0]
+]
+
+export const operations = ["+", "-", "*", "/"];
+export const equalSign = "=";
+
+
+export const Calculator = () => {
+
+  const [value, setValue] = useState("");
+  const [hasResult, setHasResult] = useState(false);
+
+  const handlerNumber = num => {
+    return () => {
+      if (hasResult) {
+        setValue(String(num));
+        setHasResult(false);
+      } else {
+        setValue(value.concat(String(num)));
+      }
+    };
+  }
+
+
+  const handlerOperation = operation => {
+    return () => {
+      setValue(String(value).concat(operation));
+      if (hasResult) setHasResult(false);
+    }
+}
+
+
+  const handleEqual = () => {
+    setValue(String(evaluate(value)));
+    setHasResult(true);
+  }
+
+  return (
+    <section>
+      <h1>Calculator</h1>
+      <input value={value} readOnly />
+      <div role="grid">
+        {
+          rows.map((row,idx) => (
+            <div key={idx} role='row'>
+              {
+                row.map(number => (
+                  <button 
+                    onClick={ handlerNumber(number) } 
+                    key={number}
+                  >
+                    { number }
+                  </button>
+                )) 
+              }
+            </div>
+          ))
+        }
+        {
+          operations.map(operation => (
+            <button 
+              onClick={ handlerOperation( operation ) } 
+              key={operation}
+            >
+              { operation }
+            </button>
+          ))
+        }
+        <button onClick={ handleEqual }>{ equalSign }</button>
+        <button onClick={ () => setValue('') }> AC </button>
+      </div>
+    </section>
+  );
+}
